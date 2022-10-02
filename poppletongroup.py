ef23ba77ef2429177f58a5ea6ce4b18d675fb9e7
anayoA <107160255+anayoA@users.noreply.github.com>
2022-10-02 21:08:23 +0100

    Add files via upload

---
+++ poppletongroup.py
@@ -0,0 +1,12 @@
+#I don't know if its three different calculations or the sum of all student numbers so I'm just gonna do the latter
+#113+175+12
+
+#number of groups needed is..
+val1 = (113 + 175 + 12) // 24
+print('The number of full groups is: ' + str(val1))
+
+#number of students left over is..
+
+val2 = (113 + 175 + 12) % 24
+print('The number of students left over in the small group is: ' + str(val2))
+
