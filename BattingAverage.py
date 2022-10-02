297d8b15032afa3084c72abcd29b63a5b2d16652
anayoA <107160255+anayoA@users.noreply.github.com>
2022-10-02 21:01:23 +0100

    Add files via upload

---
+++ BattingAverage.py
@@ -0,0 +1,8 @@
+# In a long career for Yorkshire, Geoffrey Boycott played 609 matches, batted 1014
+# times, was not out 162 times, and scored 48426 runs. Write a program to calculate
+# and display Boycott's batting average.
+
+# So its 48426 / (1014-162)
+
+avg = float(48426 / (1014 - 162))
+print('Geoffrey Boycott played 609 matches in his career, and scored a batting average of ' + (str(avg)))
