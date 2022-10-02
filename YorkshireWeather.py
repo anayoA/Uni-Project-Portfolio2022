c48ca1a3c04bfe0d4d1d0b0662269ab8b9d67aff
anayoA <107160255+anayoA@users.noreply.github.com>
2022-10-02 20:51:14 +0100

    Add files via upload

---
+++ YorkshireWeather.py
@@ -0,0 +1,6 @@
+temp = float(input('It feels hot today. What is the temperature in celsius?')) #float for multiplying with 1.8
+
+farentemp = (temp * 1.8) + 32
+
+print('Wow! In fahrenheit that is ' + str(farentemp) + 'F!') #str is so it can be printed
+
